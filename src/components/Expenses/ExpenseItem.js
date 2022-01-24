f6a094352ac8b87dd58e5b2a7c791736d1471173
Maarit Salo <p0033@student.jamk.fi>
2022-01-24 22:14:27 +0200

    reordering

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,19 @@
 import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
-  
+  const clickHandler = () => {
+    console.log("Clicked!!!")
+  }
+
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate date={props.date}/>
       <div className='expense-item__description'><h2>{props.title}</h2></div>
       <div className='expense-item__price'>${props.amount}</div>
-    </div>);
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>);
 }
 
 export default ExpenseItem;
